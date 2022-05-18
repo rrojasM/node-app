39b9b62dfa5565967a4d9ffed5070508ce240cc1
Rufino Rojas <rojasru@globalhitss.com>
2022-05-18 16:56:50 -0500

    Config database connect

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+const databaseConnect = require('./config/mongo')
 const app = express();
 
 
--- config/mongo.js
+++ config/mongo.js
@@ -1,7 +1,22 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 
+
 const databaseConnect = () => {
+
+    const DB = process.env.DB_URI;
+
+    mongoose.connect(DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }, (err, res) => {
+        if (!err) {
+            console.log("MONGODB ONLINE");
+        } else {
+            console.log("MONGODB ERROR", err);
+        }
+    });
 
 }
 
