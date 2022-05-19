1970f57fd2a29abfe942e9aa5f5354f305d0f63c
Ruf <ruf_rm@hotmail.com>
2022-05-18 23:40:08 -0500

    Change in storages

--- routes/storages.js
+++ routes/storages.js
@@ -5,7 +5,7 @@ const router = express.Router();
 //TODO: http://localhost/tracks GET POST DELETE PUT
 
 router.get("/", (req, res) => {
-    const data = ["Hola", "Storage"];
+    const data = ["Hola", "Storages"];
     res.send({ data })
 });
 
