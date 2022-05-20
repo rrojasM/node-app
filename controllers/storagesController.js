e00d00758b52b2e740d0a695f9f2d2aef891a800
Rufino Rojas <rojasru@globalhitss.com>
2022-05-20 11:52:55 -0500

    CRUD storages

--- controllers/storagesController.js
+++ controllers/storagesController.js
@@ -1,23 +1,37 @@
+const fs = require('fs');
+const { matchedData } = require('express-validator');
 const { storagesModel } = require('../models');
+const { handleHttpError } = require('../utils/handleError');
 const PUBLIC_URL = process.env.PUBLIC_URL;
+const MEDIA_PATH = `${__dirname}/../storage`;
 /**
  * Obtener una lista
  * @param {*} req 
  * @param {*} res 
  */
 const getItems = async (req, res) => {
+    try {
+        const data = await storagesModel.find({})
+        res.send({ data })
 
-    const data = await storagesModel.find({})
-    res.send({ data })
+    } catch (error) {
+        handleHttpError(res, "OCURRIO UN ERROR AL OBTENET STORAGES")
+    }
 }
-
 
 /**
  * Obtener elemento 
  * @param {*} req 
  * @param {*} res 
  */
-const getItem = (req, res) => {
+const getItem = async (req, res) => {
+    try {
+        const { id } = matchedData(req);
+        const data = await storagesModel.findById(id)
+        res.send({ data })
+    } catch (error) {
+        handleHttpError(res, "OCURRIO UN ERROR AL OBTENER STORAGE DETALLE")
+    }
 
 }
 
@@ -28,7 +42,7 @@ const getItem = (req, res) => {
  */
 const createItem = async (req, res) => {
     const { body, file } = req;
-    console.log({body, file});
+    console.log({ body, file });
     const fileData = {
         filename: file.filename || undefined,
         url: `${PUBLIC_URL}/${file.filename}`
@@ -38,11 +52,11 @@ const createItem = async (req, res) => {
 }
 
 /**
- * Actualizar Elemento
+ * Actualizar Elemento NO SE VA APLICAR EL ACTUALIZAR 
  * @param {*} req 
  * @param {*} res 
  */
-const updateItem = (req, res) => {
+const updateItem = async (req, res) => {
 
 }
 
@@ -51,7 +65,29 @@ const updateItem = (req, res) => {
  * @param {*} req 
  * @param {*} res 
  */
-const deleteItem = (req, res) => {
+const deleteItem = async (req, res) => {
+    try {
+        const { id } = matchedData(req);
+        //BUSCAMOS EL ID QUE REGRESA DE LA CONSULTA A MONGODB
+        const dataFile = await storagesModel.findById(id);
+        //ELIMINAMOS EL REGISTRO EN MONGODB
+        await storagesModel.deleteOne(id);
+
+        //OBTENEMOS LA RUTA DEL ARCHIVO MP3 PNG O JPG
+        const { filename } = dataFile;
+        //ELIMINAMOS EL REGISTRO DE MANERA FISICA QUE SE ENCUNTRA EN LA CARPETA DE STORAGE
+        const filePath = `${MEDIA_PATH}/${filename}`; //TODO:PROYECTO RUTA EN STORAGE
+        //COMENTAR ESTA LINEA PARA MANTENER EL DELETE SOFT Y EN LINEA 74 CAMBIAR EL deleteOne a delete({_id:id})
+        fs.unlinkSync(filePath);
+        //RETORNAMOS  LA RUTA DEL FILE 
+        const data = {
+            filePath,
+            deleted: 1
+        }
+        res.send({ data });
+    } catch (error) {
+        handleHttpError(res, "OCURRIO UN ERROR AL OBTENER STORAGE DETALLE")
+    }
 
 }
 
--- routes/storages.js
+++ routes/storages.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const uploadMiddleware = require('../utils/handleStorage');
-const { createItem } = require('../controllers/storagesController');
+const { createItem, getItems, updateItem, deleteItem, getItem } = require('../controllers/storagesController');
+const { validatorGetItem } = require('../validators/storages');
+/**
+ * Obtener elementos
+ */
+router.get("/", getItems);
 
+/**
+ * Obtener elemento detalle
+ */
+router.get("/:id", validatorGetItem, getItem);
+
+/**
+ * Crear elemento
+ */
 router.post("/", uploadMiddleware.single("myFile"), createItem);
+/**
+ * Actualizar elemento
+ */
+router.put("/:id", validatorGetItem, updateItem);
+/**
+ * Eliminar elemento
+ */
+router.delete("/:id", validatorGetItem, deleteItem);
 
 
 module.exports = router;
